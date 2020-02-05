d57240a1564d7203baba2fd3a66d9c41501950f2
as12334 <kejunjie1991@163.com>
2020-02-06 01:25:52 +0800

    first commit

---
+++ MesnacCoding/GlobalConfig.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MesnacCoding
+{
+    /// <summary>
+    /// »´æ÷≈‰÷√¿‡
+    /// </summary>
+    [Serializable]
+    public class GlobalConfig
+    {
+        public static ConfigItem Item = new ConfigItem();
+    }
+}
